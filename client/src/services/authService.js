b9380cdbb502ea65c2840a8ce27ff1767dbbc9e2
nina <ninagbs@abv.bg>
2023-04-18 10:43:38 +0300

    remove unnecessary slashes

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,7 +1,7 @@
 
 import * as request from './requester';
 
-const baseUrl ='https://unasiposvetasdeca-dgdi.vercel.app/';
+const baseUrl ='https://unasiposvetasdeca-dgdi.vercel.app';
     
 
 // const baseUrl = 'http://localhost:3030'; // TODO: Add server url when deployed
--- client/src/services/storyService.js
+++ client/src/services/storyService.js
@@ -2,7 +2,7 @@ import * as request from "./requester";
 
 // const baseUrl =  'http://localhost:3030'; // TODO: Add server url when deployed
 
-const baseUrl ='https://unasiposvetasdeca-dgdi.vercel.app/';
+const baseUrl ='https://unasiposvetasdeca-dgdi.vercel.app';
 const url = `${baseUrl}/data/catalog`;
 
 
