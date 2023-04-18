12d4e9fed457ca739c729572244615c7f044c1cd
nina <ninagbs@abv.bg>
2023-04-18 10:32:25 +0300

    Add cors

--- server/middlewares/cors.js
+++ server/middlewares/cors.js
@@ -1,8 +1,15 @@
-module.exports = () => (req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'HEAD, OPTIONS, GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, X-Authorization');
+// module.exports = () => (req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Methods', 'HEAD, OPTIONS, GET, POST, PUT, DELETE');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, X-Authorization');
 
     
+//     next();
+// };
+
+module.exports = () => (req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://unasiposvetasdeca.vercel.app'); // Replace with the origin of your React.js app
+    res.setHeader('Access-Control-Allow-Methods', 'HEAD, OPTIONS, GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
-};
+  };
